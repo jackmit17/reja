a699799c7f5a79291bd69dadb9c731a16bcc63fa
jackmit17 <jahongir.0776@gmail.com>
2024-08-19 09:37:40 +0900

    feat: Solution for Task-A

--- train.js
+++ train.js
@@ -1,35 +1,34 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yahshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
 
-//CALLBACK functions
-function maslahatBering(a, callback) {
-    if (typeof a !== "number") callback("insert number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a < 30) callback(null, list[1]);
-    else if (a > 30 && a < 40) callback(null, list[2]);
-    else if (a > 40 && a < 50) callback(null, list[3]);
-    else if (a > 50 && a < 60) callback(null, list[4]);
-    else {
-       setInterval(function () {
-        callback(null, list[5]);
-        }, 1000);
-    }
-  }
+// //CALLBACK functions
+// function maslahatBering(a, callback) {
+//     if (typeof a !== "number") callback("insert number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a < 30) callback(null, list[1]);
+//     else if (a > 30 && a < 40) callback(null, list[2]);
+//     else if (a > 40 && a < 50) callback(null, list[3]);
+//     else if (a > 50 && a < 60) callback(null, list[4]);
+//     else {
+//        setInterval(function () {
+//         callback(null, list[5]);
+//         }, 1000);
+//     }
+//   }
 
-maslahatBering(53, (err, data) => {
-    if(err) console.log("ERROR:", err);
-    else {
-        console.log(data);
-    }
-})
-
+// maslahatBering(53, (err, data) => {
+//     if(err) console.log("ERROR:", err);
+//     else {
+//         console.log(data);
+//     }
+// })
 
 // ASYNC function
 // async function maslahatBering(a, callback) {
@@ -48,7 +47,7 @@ maslahatBering(53, (err, data) => {
 //   }
 // }
 
-// call via then/catch 
+// call via then/catch
 // console.log('passed here 0');
 // maslahatBering(25)
 // .then((data) => {
@@ -65,3 +64,25 @@ maslahatBering(53, (err, data) => {
 //   console.log(javob);
 // }
 // run();
+
+
+// A-TASK: 
+
+// Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+
+// masalaning yechimi:
+
+function countLetter(letter, word) {
+  let count = 0;
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] == letter) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let result = countLetter('e', 'engineer');
+console.log('Result:', result);
+
