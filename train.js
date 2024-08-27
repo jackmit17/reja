172295c35cbd3104ade4f52400b5d91693d59846
jackmit17 <jahongir.0776@gmail.com>
2024-08-27 09:21:56 +0900

    feat: completion of D-TASK

--- train.js
+++ train.js
@@ -1,41 +1,16 @@
-// MITASK-C
+// D-TASK: 
 
-// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
-// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
 
-const moment = require('moment');
-const time = moment().format('HH:mm');
-
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.products = {
-      non: non,
-      lagmon: lagmon,
-      cola: cola,
-    };
+function checkContent(word, letters) {
+  let check1 = word.split("").sort().join("");
+  let check2 = letters.split("").sort().join("");
+  if (check1 === check2) {
+    return true;
   }
-
-  qoldiq() {
-    return `Hozir ${time}da ${this.products.non}ta non, ${this.products.lagmon}ta lagmon va ${this.products.cola}ta cola mavjud!`;
-  }
-
-  sotish(product, amount) {
-    if (this.products[product] >= amount) {
-      this.products[product] -= amount;
-      return `Hozir ${time}da ${amount}ta ${product} sotildi!`;
-    } else {
-      return `Hozir ${time}da ${product} yetarli miqdorda emas!`;
-    }
-  }
-
-  qabul(product, amount) {
-    this.products[product] += amount;
-    return `Hozir ${time}da ${amount}ta ${product} qabul qilindi!`;
-  }
+  return false;
 }
 
-const shop = new Shop(4, 5, 2);
-console.log(shop.qoldiq());
-console.log(shop.sotish('non', 3));
-console.log(shop.qabul('cola', 4));
-console.log(shop.qoldiq());
+const result = checkContent("mitgroup", "mitgroup");
+console.log("Result:", result);
