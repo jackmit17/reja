fa9a2cbd8176bcd62937db37be83e61a5eee941f
jackmit17 <jahongir.0776@gmail.com>
2024-08-28 12:27:27 +0900

    feat: Completion of E_TASK

--- train.js
+++ train.js
@@ -1,16 +1,13 @@
-// D-TASK: 
+// E-TASK: 
 
-// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
-// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
 
-function checkContent(word, letters) {
-  let check1 = word.split("").sort().join("");
-  let check2 = letters.split("").sort().join("");
-  if (check1 === check2) {
-    return true;
+function getReverse(word) {
+  let phase = word.split('').reverse('').join('')
+  if (phase !== undefined) {
+     return phase
   }
-  return false;
 }
-
-const result = checkContent("mitgroup", "mitgroup");
+const result = getReverse("hello")
 console.log("Result:", result);
